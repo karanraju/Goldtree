cb0d2ca6c4f7a944cb5b812cdd18dfd710934027
Sanam-Chaudhary-Code <sanamchaudhary344@gmail.com>
2025-05-24 22:02:38 +0545

    Completed Goldtree internship assignment

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,39 @@
-import styles from "./styles/app.module.css";
+import { useState } from "react";
+import { ConfigProvider, Button, Space } from "antd";
+import LoginForm from "./pages/login.form";
+import ProfileEdit from "./pages/profile.edit"
+import "./App.css";
 
-const App = () => {
-  return <div className={styles["goldtree-app"]}>Welcome to goldtree</div>;
-};
+function App() {
+  const [currentPage, setCurrentPage] = useState("login");
+
+  return (
+    <ConfigProvider >
+      <div className="app">
+        {/* Navigation */}
+        <div className="app-navigation">
+          <Space>
+            <Button
+              type={currentPage === "login" ? "primary" : "default"}
+              onClick={() => setCurrentPage("login")}
+            >
+              Login
+            </Button>
+            <Button
+              type={currentPage === "profile" ? "primary" : "default"}
+              onClick={() => setCurrentPage("profile")}
+            >
+              Profile
+            </Button>
+          </Space>
+        </div>
+
+        {/* Pages */}
+        {currentPage === "login" && <LoginForm />}
+         {currentPage === "profile" && <ProfileEdit />}
+      </div>
+    </ConfigProvider>
+  );
+}
 
 export default App;
